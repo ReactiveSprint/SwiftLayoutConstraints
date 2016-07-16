2e526edd80c357a1a17b7350670fa0dd64c8a533
Ahmad Baraka <ah-baraka@live.com>
2016-07-16 13:22:54 +0200

    Pod lib create

---
+++ Example/Pods/Target Support Files/Pods-SwiftLayoutConstraints_Example/Pods-SwiftLayoutConstraints_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftLayoutConstraints_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftLayoutConstraints_Example
+@end
---
+++ Example/Pods/Target Support Files/SwiftLayoutConstraints/SwiftLayoutConstraints-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SwiftLayoutConstraints : NSObject
+@end
+@implementation PodsDummy_SwiftLayoutConstraints
+@end
