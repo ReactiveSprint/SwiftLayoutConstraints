5f6b38797895fa68fd48145e8319b2adef9e93b6
Ahmad Baraka <ah-baraka@live.com>
2016-07-22 07:19:12 +0200

    Removed Example project

---
+++ Example/Tests/LayoutConstraintTypeSpec.swift
@@ -0,0 +1,16 @@
+//
+//  LayoutConstraintTypeSpec.swift
+//  SwiftLayoutConstraints
+//
+//  Created by Ahmad Baraka on 7/21/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import Nimble
+import Quick
+import SwiftLayoutConstraints
+
+class LayoutConstraintTypeSpec: QuickSpec {
+    override func spec() {
+    }
+}
