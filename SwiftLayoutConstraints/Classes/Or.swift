30ca1d492824c0cf4b4c1613d35c8340d3ce0f61
Ahmad Baraka <ah-baraka@live.com>
2016-07-22 14:07:12 +0200

    Added Or for array & constraint

--- SwiftLayoutConstraints/Classes/Or.swift
+++ SwiftLayoutConstraints/Classes/Or.swift
@@ -11,13 +11,24 @@ import Foundation
 /// Creates a new `LayoutConstraintType` with all values from `lhs` and attribute `rhs`
 /// then returns an array with `rhs` value and the created instance.
 ///
-/// This is a shortcut to creating multiple `LayoutConstraintType` 
+/// This is a shortcut to creating multiple `LayoutConstraintType`
 /// sharing same values except their `attribute.`
 @warn_unused_result
 public func | <L: LayoutConstraintType>(lhs: L, rhs: NSLayoutAttribute) -> [L] {
-    var second = lhs
-    second.attribute = rhs
-    return [lhs, second]
+    return [lhs] | rhs
+}
+
+/// Creates a new `LayoutConstraintType` with all values from first element of `lhs`
+/// and attribute `rhs` then appends the new constraint.
+///
+/// This is a shortcut to creating multiple `LayoutConstraintType`
+/// sharing same values except their `attribute.`
+@warn_unused_result
+public func | <L: LayoutConstraintType>(lhs: [L], rhs: NSLayoutAttribute) -> [L] {
+    precondition(!lhs.isEmpty)
+    var constraint = lhs.first!
+    constraint.attribute = rhs
+    return lhs | constraint
 }
 
 /// Returns an array with `lhs` and `rhs.`
@@ -26,14 +37,37 @@ public func | <L: LayoutConstraintType>(lhs: L, rhs: L) -> [L] {
     return [lhs, rhs]
 }
 
+/// Returns an array by adding `rhs` and `lhs.`
+@warn_unused_result
+public func | <L: LayoutConstraintType>(lhs: [L], rhs: L) -> [L] {
+    precondition(!lhs.isEmpty)
+    var lhs = lhs
+    lhs.append(rhs)
+    return lhs
+}
+
 /// Returns an array with `lhs` and `rhs.`
 @warn_unused_result
 public func | <Value: AnyObject, L: LayoutConstraintType where L.Value == Value>(lhs: L, rhs: RhsLayoutConstraint<Value>) -> [RhsLayoutConstraint<Value>] {
     return [RhsLayoutConstraint(constraint: lhs), rhs]
 }
 
+/// Returns an array with `lhs` and `rhs` contents.
+@warn_unused_result
+public func | <Value: AnyObject, L: LayoutConstraintType where L.Value == Value>(lhs: L, rhs: [RhsLayoutConstraint<Value>]) -> [RhsLayoutConstraint<Value>] {
+    var array = [RhsLayoutConstraint(constraint: lhs)]
+    array.appendContentsOf(rhs)
+    return array
+}
+
 /// Returns an array with `lhs` and `rhs.`
 @warn_unused_result
 public func | <Value: AnyObject, L: LayoutConstraintType where L.Value == Value>(lhs: RhsLayoutConstraint<Value>, rhs: L) -> [RhsLayoutConstraint<Value>] {
     return [lhs, RhsLayoutConstraint(constraint: rhs)]
+}
+
+/// Returns an array by adding `rhs` to `lhs.`
+@warn_unused_result
+public func | <Value: AnyObject, L: LayoutConstraintType where L.Value == Value>(lhs: [RhsLayoutConstraint<Value>], rhs: L) -> [RhsLayoutConstraint<Value>] {
+    return lhs | RhsLayoutConstraint(constraint: rhs)
 }
