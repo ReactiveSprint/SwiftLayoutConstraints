a8fe546daed047e6f0517bdbac39e3d88a8c3d9e
Ahmad Baraka <ah-baraka@live.com>
2016-07-24 06:09:26 +0200

    NSLayoutConstraint with activate parameter

--- Example/Tests/NSLayoutConstraintSpec.swift
+++ Example/Tests/NSLayoutConstraintSpec.swift
@@ -15,6 +15,19 @@ class NSLayoutConstraintSpec: QuickSpec {
         describe("init with constraints") {
             var lhs: LhsLayoutConstraint<NSObject>!
             var constraint: NSLayoutConstraint!
+            var view1: UIView!
+            var view2: UIView!
+            var parent: UIView!
+            
+            beforeEach {
+                view1 = UIView()
+                view2 = UIView()
+                parent = UIView()
+                parent.addSubview(view1)
+                parent.addSubview(view2)
+                
+                lhs = LhsLayoutConstraint(view1, attribute: NSLayoutAttribute.Bottom)
+            }
             
             afterEach {
                 expect(constraint.firstItem as? NSObject) == lhs.object
@@ -22,12 +35,17 @@ class NSLayoutConstraintSpec: QuickSpec {
             }
             
             it("should init with two lhs") {
-                let view1 = UIView()
-                let view2 = UIView()
-                let parent = UIView()
-                parent.addSubview(view1)
-                parent.addSubview(view2)
-                lhs = LhsLayoutConstraint(view1, attribute: NSLayoutAttribute.Bottom)
+                let rhs = LhsLayoutConstraint(view2, attribute: NSLayoutAttribute.Top)
+                
+                constraint = NSLayoutConstraint(lhs: lhs, rhs: rhs, relation: NSLayoutRelation.Equal, activate: false)
+                
+                expect(constraint.secondItem as? UIView) == view2
+                expect(constraint.secondAttribute) == NSLayoutAttribute.Top
+                expect(constraint.relation) == NSLayoutRelation.Equal
+                expect(constraint.active) == false
+            }
+            
+            it("should init with two lhs and activate") {
                 let rhs = LhsLayoutConstraint(view2, attribute: NSLayoutAttribute.Top)
                 
                 constraint = NSLayoutConstraint(lhs: lhs, rhs: rhs, relation: NSLayoutRelation.Equal)
@@ -35,15 +53,23 @@ class NSLayoutConstraintSpec: QuickSpec {
                 expect(constraint.secondItem as? UIView) == view2
                 expect(constraint.secondAttribute) == NSLayoutAttribute.Top
                 expect(constraint.relation) == NSLayoutRelation.Equal
+                expect(constraint.active) == true
             }
             
             it("should init with rhs") {
-                let view1 = UIView()
-                let view2 = UIView()
-                let parent = UIView()
-                parent.addSubview(view1)
-                parent.addSubview(view2)
-                lhs = LhsLayoutConstraint(view1, attribute: NSLayoutAttribute.Bottom)
+                let rhs = RhsLayoutConstraint(view2, attribute: NSLayoutAttribute.Top, constant: -10, multiplier: 0.5)
+                
+                constraint = NSLayoutConstraint(lhs: lhs, rhs: rhs, relation: NSLayoutRelation.GreaterThanOrEqual, activate: false)
+                
+                expect(constraint.secondItem as? UIView) == view2
+                expect(constraint.secondAttribute) == NSLayoutAttribute.Top
+                expect(constraint.relation) == NSLayoutRelation.GreaterThanOrEqual
+                expect(constraint.constant) == -10
+                expect(constraint.multiplier) == 0.5
+                expect(constraint.active) == false
+            }
+            
+            it("should init with rhs and activate") {
                 let rhs = RhsLayoutConstraint(view2, attribute: NSLayoutAttribute.Top, constant: -10, multiplier: 0.5)
                 
                 constraint = NSLayoutConstraint(lhs: lhs, rhs: rhs, relation: NSLayoutRelation.GreaterThanOrEqual)
@@ -53,6 +79,7 @@ class NSLayoutConstraintSpec: QuickSpec {
                 expect(constraint.relation) == NSLayoutRelation.GreaterThanOrEqual
                 expect(constraint.constant) == -10
                 expect(constraint.multiplier) == 0.5
+                expect(constraint.active) == true
             }
         }
     }
--- SwiftLayoutConstraints/Classes/NSLayoutConstraint.swift
+++ SwiftLayoutConstraints/Classes/NSLayoutConstraint.swift
@@ -15,8 +15,9 @@ public extension NSLayoutConstraint {
     /// - Parameter rhs: Represents `secondItem` values.
     ///                  `constant` and `mutliplier` values are used to create this instance.
     ///                  Can be any `LayoutConstraintType.`
+    /// - Parameter relation: `NSLayoutRelation` used to init to the `NSLayoutConstraint.`
     public convenience init<Value1: AnyObject, Value2: AnyObject, L2: LayoutConstraintType where L2.Value == Value2>(lhs: LhsLayoutConstraint<Value1>, rhs: L2, relation: NSLayoutRelation) {
-        self.init(lhs: lhs, rhs: RhsLayoutConstraint(constraint: rhs), relation: relation)
+        self.init(lhs: lhs, rhs: RhsLayoutConstraint(constraint: rhs), relation: relation, activate: true)
     }
     
     /// Initializes and **activates** `NSLayoutConstraint` instance with values from `LayoutConstraintType.`
@@ -25,7 +26,32 @@ public extension NSLayoutConstraint {
     /// - Parameter rhs: Represents `secondItem` values.
     ///                  `constant` and `mutliplier` values are used to create this instance.
     ///                  Can be any `LayoutConstraintType.`
+    /// - Parameter relation: `NSLayoutRelation` used to init to the `NSLayoutConstraint.`
     public convenience init<Value1: AnyObject, Value2: AnyObject, L2: LayoutConstraintType where L2.Value == Value2?>(lhs: LhsLayoutConstraint<Value1>, rhs: L2, relation: NSLayoutRelation) {
+        self.init(lhs: lhs, rhs: RhsLayoutConstraint(constraint: rhs), relation: relation, activate: true)
+    }
+    
+    /// Initializes `NSLayoutConstraint` instance with values from `LayoutConstraintType.`
+    ///
+    /// - Parameter lhs: Represents `firstItem` values. Must always be `LhsLayoutConstraint.`
+    /// - Parameter rhs: Represents `secondItem` values.
+    ///                  `constant` and `mutliplier` values are used to create this instance.
+    ///                  Can be any `LayoutConstraintType.`
+    /// - Parameter relation: `NSLayoutRelation` used to init to the `NSLayoutConstraint.`
+    /// - Parameter activate: if `true`, the receiver will be activated immediately.
+    public convenience init<Value1: AnyObject, Value2: AnyObject, L2: LayoutConstraintType where L2.Value == Value2>(lhs: LhsLayoutConstraint<Value1>, rhs: L2, relation: NSLayoutRelation, activate: Bool) {
+        self.init(lhs: lhs, rhs: RhsLayoutConstraint(constraint: rhs), relation: relation, activate: activate)
+    }
+    
+    /// Initializes `NSLayoutConstraint` instance with values from `LayoutConstraintType.`
+    ///
+    /// - Parameter lhs: Represents `firstItem` values. Must always be `LhsLayoutConstraint.`
+    /// - Parameter rhs: Represents `secondItem` values.
+    ///                  `constant` and `mutliplier` values are used to create this instance.
+    ///                  Can be any `LayoutConstraintType.`
+    /// - Parameter relation: `NSLayoutRelation` used to init to the `NSLayoutConstraint.`
+    /// - Parameter activate: if `true`, the receiver will be activated immediately.
+    public convenience init<Value1: AnyObject, Value2: AnyObject, L2: LayoutConstraintType where L2.Value == Value2?>(lhs: LhsLayoutConstraint<Value1>, rhs: L2, relation: NSLayoutRelation, activate: Bool) {
         self.init(item: lhs.object,
                   attribute: lhs.attribute,
                   relatedBy: relation,
@@ -33,6 +59,9 @@ public extension NSLayoutConstraint {
                   attribute: rhs.attribute,
                   multiplier: rhs.multiplier,
                   constant: rhs.constant)
-        active = true
+        
+        if(activate) {
+            active = true
+        }
     }
 }
